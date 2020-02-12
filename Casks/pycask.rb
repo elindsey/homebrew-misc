a5c26997a40ab723cc073041ae81f91d7afef0f1
Eli Lindsey <eli@siliconsprawl.com>
2020-02-11 22:50:53 -0800

    wip cask

---
+++ Casks/pycask.rb
@@ -0,0 +1,22 @@
+cask 'pycask' do
+  version '2.7.17'
+  sha256 '93791aedab6f9e708fb97d2e4ac3a2f15e7acbbb1881d1021f2dd785c622c484'
+
+  url "https://www.python.org/ftp/python/#{version}/python-#{version}-macosx10.9.pkg"
+  name 'Python 2'
+  homepage 'https://python.org/'
+
+  depends_on macos: '>= :mavericks'
+
+  pkg "python-#{version}-macosx10.9.pkg"
+
+  uninstall delete: [
+                      "/Library/Receipts/Python*-#{version.slice(%r{\d+\.\d+})}.pkg",
+                      "/Applications/Python #{version.slice(%r{\d+\.\d+})}",
+                      "/Library/Frameworks/Python.Framework/Versions/#{version.slice(%r{\d+\.\d+})}",
+                      "/Library/Python/#{version.slice(%r{\d+\.\d+})}",
+                    ]
+
+  zap delete: "~/Library/Python/#{version.slice(%r{\d+\.\d+})}"
+
+end
